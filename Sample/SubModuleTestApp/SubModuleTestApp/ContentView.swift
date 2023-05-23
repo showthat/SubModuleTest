56ac9df13c2bc5a6abfb84c0b7de09c1868e5701
Hoon <>
2023-05-23 16:42:48 +0900

    Feat: Update Sample App

--- Sample/SubModuleTestApp/SubModuleTestApp/ContentView.swift
+++ Sample/SubModuleTestApp/SubModuleTestApp/ContentView.swift
@@ -6,16 +6,29 @@
 //
 
 import SwiftUI
+import TestMainFramework
+
 
 struct ContentView: View {
     var body: some View {
+        
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
+            
             Text("Hello, world!")
+                .padding()
+            
+            Text(TestMainSDK.shared.getLog())
+                .multilineTextAlignment(.center)
         }
         .padding()
+        .onAppear {
+            TestMainSDK.shared.printLog()
+        }
+        
     }
 }
 
