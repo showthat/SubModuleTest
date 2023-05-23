dc4bcc3bab860501eef8371f1fedc274da3ad5c7
Hoon <>
2023-05-23 16:35:19 +0900

    add sample app

---
+++ Sample/SubModuleTestApp/SubModuleTestApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SubModuleTestApp
+//
+//  Created by 이동훈 on 2023/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sample/SubModuleTestApp/SubModuleTestApp/SubModuleTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubModuleTestAppApp.swift
+//  SubModuleTestApp
+//
+//  Created by 이동훈 on 2023/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SubModuleTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
