c6dfbeed640f38b0a3690126ced3183e7ef69f16
dhlee <dh.lee@mobwith.co.kr>
2025-12-30 16:39:59 +0900

    spm 수정

--- Package.swift
+++ Package.swift
@@ -8,12 +8,24 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SubModuleTest",
-            targets: ["TestMainFramework", "TestPrintGoodbyeFramework", "TestPrintHelloFramework"]),
+            name: "Core",
+            targets: ["TestMainFramework"]),
+        .library(
+            name: "Goodbye",
+            targets: ["GoodbyeWrapper"]),
+        .library(
+            name: "Hello",
+            targets: ["HelloWrapper"])
+        // cocoapod 또는 xcframewok로 된 외부 프레임워크는 내것 추가하듯이 하면 됨.
+        // cocoapod는 외부 프레임워크 다운받아서 사용, 다운 못 받으면 cocoapod로 배포 고려해야함
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+// SPM으로된 외부 SDK 의존성 추가시..
+//        .package(
+//                url: "https://github.com/some/ExternalSDK.git",
+//                from: "1.2.0"
+//            )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -30,5 +42,25 @@ let package = Package(
             name: "TestPrintHelloFramework",
             path: "Sources/TestPrintHelloFramework.xcframework"
         ),
+        .target(
+            name: "HelloWrapper",
+            dependencies: [
+                "TestMainFramework",
+                "TestPrintHelloFramework"
+            ]
+        ),
+        .target(
+            name: "GoodbyeWrapper",
+            dependencies: [
+                "TestMainFramework",
+                "TestPrintGoodbyeFramework"
+                
+                // SPM으로된 외부 SDK 의존성 추가시..
+//                .product(
+//                    name: "ExternalSDK",
+//                    package: "ExternalSDK"
+//                )
+            ]
+        )
     ]
 )
---
+++ Sources/GoodbyeWrapper/Empty.swift
@@ -0,0 +1,7 @@
+//
+//  Empty.swift
+//  SubModuleTest
+//
+//  Created by 이동훈 on 12/30/25.
+//
+
